d1f58b9ac1e762bf5b71c8b4b60d1338f2499ade
aman4718 <bholukumar4718@gmail.com>
2024-12-17 21:56:44 +0530

    xdisplay name

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import InputForm from "./InputForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <InputForm/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/InputForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const InputForm = () => {
+    const [formData,setFormData] = useState({
+        firstname:'',
+        lastname:''
+    });
+    const [submitData,setSubmitData] = useState(null);
+
+    const handleChange = (e) => {
+        const {name,value} = e.target;
+        setFormData({
+            ...formData,
+            [name]:value
+        })
+
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitData(formData);
+        setFormData({firstname:'',lastname:''})
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h1>Full Name Display</h1>
+                    <div>
+                        <label>First Name:</label>
+                        <input type="text" id="firstname" name="firstname" value={formData.firstname} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <label>Last Name:</label>
+                        <input type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                    {submitData && (
+                        <div>
+                            <p>Full Name: {submitData.firstname} {submitData.lastname}</p>
+                        </div>
+                    )}
+                </div>
+            </form>
+        </div>
+    )
+}
+export default InputForm;
